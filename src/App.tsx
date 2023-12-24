ca1c5a0f6ed3b1dcf82a665a6670b81a19fa4ff5
giuseppe zammuto <mrczammuto@gmail.com>
2023-12-24 11:25:15 +0100

    React Calculator

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Calculator from "./Calculator/Calculator.tsx";
+
+function App() {
+  return (
+    <>
+      <Calculator></Calculator>
+    </>
+  );
+}
+
+export default App;
